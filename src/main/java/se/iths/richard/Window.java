ebc59a64879aa2bb878c2f7c5613b26b9fd04e6c
Richard <54506990+RichLarIva@users.noreply.github.com>
2026-09-18 10:28:29 +0200

    Attempt to run

--- src/main/java/se/iths/richard/Window.java
+++ src/main/java/se/iths/richard/Window.java
@@ -1,15 +1,22 @@
 package se.iths.richard;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
     private static Window window = null;
     private int width, height;
     private String title;
+    private long glfwWindow;
 
     private Window() {
-        this.width = 1920;
-        this.height = 1080;
+        this.width = 2560;
+        this.height = 1440;
         this.title = "Best Tamogotchi 2D";
     }
 
@@ -46,5 +53,55 @@ public class Window {
 
     public void run() {
         IO.println("Hello LWJGL" + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW!");
+
+
+        glfwDefaultWindowHints();
+
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // OpenGL 3.3
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // enable vsync
+        glfwSwapInterval(1);
+
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // poll events
+            glfwPollEvents();
+
+            glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 }
